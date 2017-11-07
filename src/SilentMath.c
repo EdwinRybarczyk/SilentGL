b15ee3a42558be9a0b0dd9e420af500c06534d3f
EdwinRy <edwinrybarczyk4d@gmail.com>
2017-11-07 12:06:35 +0000

    ok

--- src/SilentMath.c
+++ src/SilentMath.c
@@ -1,26 +1,26 @@
 #include "SilentMath.h"
-vec2i* createVec2i(int x, int y)
+vec2i createVec2i(int x, int y)
 {
-	vec2i *vec = malloc(sizeof(vec2i));
-	vec->x = x;
-	vec->y = y;
+	vec2i vec;
+	vec.x = x;
+	vec.y = y;
 	return vec;
 }
 
-vec2f* createVec2f(float x, float y)
+vec2f createVec2f(float x, float y)
 {
-	vec2f *vec = malloc(sizeof(vec2f));
-	vec->x = x;
-	vec->y = y;
+	vec2f vec;
+	vec.x = x;
+	vec.y = y;
 	return vec;
 }
 
-vec3i* createVec3i(int x, int y, int z)
+vec3i createVec3i(int x, int y, int z)
 {
-	vec3i *vec = malloc(sizeof(vec3i));
-	vec->x = x;
-	vec->y = y;
-	vec->z = z;
+	vec3i vec;
+	vec.x = x;
+	vec.y = y;
+	vec.z = z;
 	return vec;
 }
 
@@ -33,35 +33,35 @@ vec3f createVec3f(float x, float y, float z)
 	return vec;
 }
 
-vec4i* createVec4i(int x, int y, int z, int w)
+vec4i createVec4i(int x, int y, int z, int w)
 {
-	vec4i *vec = malloc(sizeof(vec4i));
-	vec->x = x;
-	vec->y = y;
-	vec->z = z;
-	vec->w = w;
+	vec4i vec;
+	vec.x = x;
+	vec.y = y;
+	vec.z = z;
+	vec.w = w;
 	return vec;
 }
-vec4f* createVec4f(float x, float y, float z, float w)
+vec4f createVec4f(float x, float y, float z, float w)
 {
-	vec4f *vec = malloc(sizeof(vec4f));
-	vec->x = x;
-	vec->y = y;
-	vec->z = z;
-	vec->w = w;
+	vec4f vec;
+	vec.x = x;
+	vec.y = y;
+	vec.z = z;
+	vec.w = w;
 	return vec;
 }
 
-mat4i* createMat4i()
+mat4i createMat4i()
 {
 	mat4i mat;
-	return &mat;
+	return mat;
 }
 
-mat4f* createMat4f()
+mat4f createMat4f()
 {
 	mat4f mat;
-	return &mat;
+	return mat;
 }
 
 float min3f(float x, float y, float z)
--- src/SilentMath.h
+++ src/SilentMath.h
@@ -7,7 +7,7 @@ typedef struct vec2i
 	int x,y;
 }vec2i;
 
-vec2i* createVec2i(int x, int y);
+vec2i createVec2i(int x, int y);
 
 //2 float vector
 typedef struct vec2f
@@ -15,7 +15,7 @@ typedef struct vec2f
 	float x,y;
 }vec2f;
 
-vec2f* createVec2f(float x, float y);
+vec2f createVec2f(float x, float y);
 
 //3 integer vector
 typedef struct vec3i
@@ -23,7 +23,7 @@ typedef struct vec3i
 	int x,y,z;
 }vec3i;
 
-vec3i* createVec3i(int x, int y, int z);
+vec3i createVec3i(int x, int y, int z);
 
 //3 float vector
 typedef struct vec3f
@@ -39,7 +39,7 @@ typedef struct vec4i
 	int x,y,z,w;
 }vec4i;
 
-vec4i* createVec4i(int x, int y, int z, int w);
+vec4i createVec4i(int x, int y, int z, int w);
 
 //4 float vector
 typedef struct vec4f
@@ -47,7 +47,7 @@ typedef struct vec4f
 	float x,y,z,w;
 }vec4f;
 
-vec4f* createVec4f(float x, float y, float z, float w);
+vec4f createVec4f(float x, float y, float z, float w);
 
 //Integer matrix 4x4
 typedef struct mat4i
@@ -55,7 +55,7 @@ typedef struct mat4i
 	int mat[4][4];
 }mat4i;
 
-mat4i* createMat4i();
+mat4i createMat4i();
 
 //Float matrix 4x4
 typedef struct mat4f
@@ -63,8 +63,8 @@ typedef struct mat4f
 	float mat[4][4];
 }mat4f;
 
-mat4f* createMat4f();
-mat4f* createIdentity();
+mat4f createMat4f();
+mat4f createIdentity();
 
 //Smallest value out of 3 floats
 float min3f(float x, float y, float z);
